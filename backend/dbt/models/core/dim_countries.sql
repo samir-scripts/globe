fe7149398e0a37f4ddb421c197b0f61cf392842d
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-05-18 18:42:34 -0400

    Setitng up Hasura for live database updates to the frontend using websockets. Postgis for the geospatial coordinates

--- backend/dbt/models/core/dim_countries.sql
+++ backend/dbt/models/core/dim_countries.sql
@@ -1,8 +1,17 @@
-with seed_data as (
+with seed_continent as (
     select * from {{ ref('iso3_to_continent') }}
+),
+
+seed_coords as (
+    select * from {{ ref('iso3_to_coords') }}
 )
 
 select
-    iso3,
-    continent
-from seed_data
+    c.iso3,
+    c.continent,
+    co.latitude,
+    co.longitude,
+    -- Generate a PostGIS Point geometry (SRID 4326 for WGS84)
+    ST_SetSRID(ST_MakePoint(co.longitude, co.latitude), 4326) as geom
+from seed_continent c
+left join seed_coords co on c.iso3 = co.iso3
